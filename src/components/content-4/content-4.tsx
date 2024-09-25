7b81671276bc9b850f66ed1eb89fdc240745be46
Mark Kahn <m-github@vouchr.com>
2024-09-25 17:33:18 +0200

    fixing org logo

--- src/components/content-4/content-4.tsx
+++ src/components/content-4/content-4.tsx
@@ -7,7 +7,12 @@ interface Content4Props {
 export default function Content4(_: Content4Props) {
   return (
     <div id="content-4" className="slider-content">
-      <img id="org-logo" src="images/org-logo.png" alt="" />
+      <img
+        id="org-logo"
+        src={process.env.PUBLIC_URL + '/images/little-angels-service-dogs.png'}
+        alt=""
+        width="100%"
+      />
     </div>
   );
 }
